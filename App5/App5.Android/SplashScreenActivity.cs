09e6fb0389208e24f7318f6ab7a95b2b7c833846
Tiago Martinho <80893376+tiagomartinhoo@users.noreply.github.com>
2023-04-17 02:41:55 +0100

    Splash Screen already works on Android.

---
+++ App5/App5.Android/SplashScreenActivity.cs
@@ -0,0 +1,40 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.companyname.app5
+{
+    [Activity(Label = "SplashScreenActivity", Theme = "@style/MyTheme.SplashScreen", MainLauncher = true, NoHistory = true)]
+    public class SplashScreenActivity : Activity
+    {
+        public override void OnCreate(Bundle savedInstanceState, PersistableBundle persistentState)
+        {
+            base.OnCreate(savedInstanceState, persistentState);
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            Task startupWork = new Task(() => { SimulateStartup(); });
+            startupWork.Start();
+        }
+
+        // Prevent the back button from canceling the startup process
+        public override void OnBackPressed() { }
+
+        // Simulates background work that happens behind the splash screen
+        async void SimulateStartup()
+        {
+            await Task.Delay(1000); // Simulate a bit of startup work.
+            StartActivity(new Intent(Application.Context, typeof(App5.Droid.MainActivity)));
+        }
+    }
+}
