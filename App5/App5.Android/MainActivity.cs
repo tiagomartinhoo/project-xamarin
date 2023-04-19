9b8d3bea01d4dd5d41bd4d1e0cd0a6ef15cc429d
Tiago Martinho <80893376+tiagomartinhoo@users.noreply.github.com>
2023-04-19 12:18:27 +0100

    Addition of the App Icon.

--- App5/App5.Android/MainActivity.cs
+++ App5/App5.Android/MainActivity.cs
@@ -6,7 +6,7 @@ using Android.OS;
 namespace App5.Droid
 {
 
-    [Activity(Label = "App5", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "App5", Icon = "@mipmap/app_icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
 
